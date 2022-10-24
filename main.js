7293317868844df6d1909681237d9cf3cd9d8e53
Aquilesdevweb <aquilesnetto2002@gmail.com>
2022-10-24 13:21:18 -0300

    cocrrigindo seletor imagem

--- main.js
+++ main.js
@@ -12,12 +12,11 @@ $(document).ready(function(){
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();//trás o valor da nova imagem (js: var = document.queryselector('seletor'.value))
         const novoItem = $('<li style="display> none"></li>'); // cria uma nova linha no HTML / para aplicar efeito dele aparecendo o estilo precisa estar none (abaixo o fadeIn)
         $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem); // inseri a imagem dentro da li que foi criada
-        $(`
-            <div class="overlay-image-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real
-                </a>
-            </div>            
-        `).appendTo(novoItem); // Inseri a formatação na li
+        $(`<div class="overlay-imagem-link">
+        <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+        Ver imagem em tamanho real
+        </a>
+        </div>`).appendTo(novoItem); // Inserir a formatação na li
         $(novoItem).appendTo('ul'); //Inseri a li dentro da ul que foi criada
         $(novoItem).fadeIn(); //Efeito de aparecimento / é uma função
         $('#endereco-imagem-nova').val(''); // Torna o valor do campo vazio depois do submit
