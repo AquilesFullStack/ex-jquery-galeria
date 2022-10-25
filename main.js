ae2fcbc4b2c65ac70ca7c176875cdaeb99897488
José Aquiles <jose.aquiles@ebac.art.br>
2022-10-25 20:33:18 -0300

    inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+$('form').on('submit', function(e){
+    e.preventDefault();
+    const campoTarefa = $('#campo-tarefa').val();
+    const novoItem = $('<li></li>');
+    $(`
+    <div id="addli">
+        <li>${campoTarefa}</li>
+    </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $('#campo-tarefa').val('');
+
+    $('#addli').click(function(){
+        $('<div id="addli" style="text-decoration:line-through"></div>').appendTo(novoItem);
+    })
+})
+
